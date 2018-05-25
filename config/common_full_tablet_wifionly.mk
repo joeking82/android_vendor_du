011233ad0aac06e86d90162b857f73bf8adc5e61
Joe pacino <37958573+joeking82@users.noreply.github.com>
2018-05-24 20:48:32 -0400

    Update common_full_tablet_wifionly.mk

--- config/common_full_tablet_wifionly.mk
+++ config/common_full_tablet_wifionly.mk
@@ -1,2 +1,7 @@
 # Inherit common stuff
 $(call inherit-product, vendor/du/config/common.mk)
+
+# Copy Magisk zip
+PRODUCT_COPY_FILES += \
+    vendor/du/prebuilt/common/magisk.zip:system/addon.d/magisk.zip
+
